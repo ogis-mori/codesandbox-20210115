0c4712b1c21edf1bd513599e2cd5e0b4fb5e0dba
ogis-mori <mori_miki@ogis-ri.co.jp>
2021-01-15 13:59:00 +0900

    step7

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,15 @@ const App = () => {
     color: "blue",
     fontsize: "18px"
   };
+  const contentLadyStyle = {
+    color: "pink",
+    fontsize: "18px"
+  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは!</h1>
       <p style={contentStyle}>お元気ですか？</p>
+      <p style={contentLadyStyle}>元気です</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );

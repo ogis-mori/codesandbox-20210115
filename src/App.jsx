99b3241a103c011f193ed8cf33aa169e7b10ff49
ogis-mori <mori_miki@ogis-ri.co.jp>
2021-01-15 13:55:04 +0900

    step6

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const App = () => {
   const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontsize: "18px"
+  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは!</h1>
-      <p>おげんきですか？</p>
+      <p style={contentStyle}>お元気ですか？</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );

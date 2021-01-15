12f2e8fd0e5feef0d44122c6c592c2f8bc111804
ogis-mori <mori_miki@ogis-ri.co.jp>
2021-01-15 14:49:05 +0900

    step13

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-
-  const [num, setNum] = useState(0);
 
   return (
     <>
@@ -14,7 +15,10 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(*´ω｀)</p>}
     </>
   );
 };

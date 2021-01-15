d92a7da9dfca28f0694e0a7d8bde2d8fa56d98fd
ogis-mori <mori_miki@ogis-ri.co.jp>
2021-01-15 14:11:39 +0900

    step9

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,11 @@ import ColorfulMessage from "./components/ColorfulMessage";
 const App = () => {
   const onClickButton = () => alert();
 
-  const contentLadyStyle = {
-    color: "pink",
-    fontsize: "18px"
-  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは!</h1>
       <ColorfulMessage color="blue" message="お元気ですか？" />
-      <p style={contentLadyStyle}>元気です</p>
+      <ColorfulMessage color="pink" message="元気です" />
       <button onClick={onClickButton}>ボタン</button>
     </>
   );

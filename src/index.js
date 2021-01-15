564789d2b02a58533227a8a15f0168dcac5ed181
ogis-mori <mori_miki@ogis-ri.co.jp>
2021-01-15 13:39:44 +0900

    step3

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDom from "react-dom";
 
 const App = () => {
   return (
-    <React.Fragment>
+    <>
       <h1>こんにちは!</h1>
       <p>おげんきですか？</p>
-    </React.Fragment>
+    </>
   );
 };
 
